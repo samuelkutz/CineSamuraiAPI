c69344b111fd3fef526d5c3e4bdfda6fcebcf8f4
ericafernandaj <eriicafernandaj@gmail.com>
2023-09-15 18:24:48 -0300

    feat: Inserção da class e constructor no arquivo SalaModel

---
+++ src/models/SalaModel.js
@@ -0,0 +1,12 @@
+class SalaModel {
+
+    // @param { string } = capacidadel
+    // @param { string } = nome_sala
+
+    constructor(capacidadel, nome_sala) {
+        isso.capacidadel = capacidadel
+        isso.nome_sala = nome_sala
+    }
+}
+
+export default SalaModel
