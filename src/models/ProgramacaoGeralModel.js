d665b8c57c164594f8e894fa5cafbf1321d37867
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-09-15 18:11:54 -0300

    feat: Inserção de class e constructor no arquivo ProgramacaoGeralModel.js

---
+++ src/models/ProgramacaoGeralModel.js
@@ -0,0 +1,12 @@
+
+class ProgramacaoGeralModel{
+// @param {string} data_horario
+// @param {string} tipo_linguagem
+
+constructor(id_filme_fk, data_horario,tipo_linguagem){
+this.data_horaio = data_horario
+this.tipo_linguagem = tipo_linguagem
+}
+}
+
+export default ProgramacaoGeralModel
