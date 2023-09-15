912d678023bb06fa43760b51e8faf98803fbc07a
ericafernandaj <eriicafernandaj@gmail.com>
2023-09-15 18:29:06 -0300

    feat: Inserção da class e constructor do arquivo PoltronaModel

---
+++ src/models/PoltronaModel.js
@@ -0,0 +1,12 @@
+class PoltronaModel {
+
+    // @param { string } = tipo_poltrona
+    // @param { string } = disponibilidade
+
+    construtor(tipo_poltrona, disponibilidade) {
+        isso.tipo_poltrona = tipo_poltrona
+        isso.disponibilidade = disponibilidade
+    }
+}
+
+export default PoltronaModel
