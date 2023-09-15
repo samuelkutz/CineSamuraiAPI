180a2b70c46d541232ff6a76e933877d8c884f87
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-09-15 18:03:29 -0300

    feat: Inserção da class e constructor

---
+++ src/models/FilmesModel.js
@@ -0,0 +1,13 @@
+class EnderecoModel{
+    // @param {string} = nome_filme
+    // @param {string} = linguagem_original
+    // @param {string} = classificacao_indicativa
+    // @param {string} = duracao
+    
+    constructor(nome_filme, linguagem_original, classificacao_indicativa, duracao){
+    this.nome_filmes = nome_filme
+    this.linguagem_original = linguagem_original
+    this.classificacao_indicativa = classificacao_indicativa
+    this.duracao = duracao
+    }
+    }
