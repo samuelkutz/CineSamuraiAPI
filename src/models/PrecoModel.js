591e6209854bc7a295813f113b211ebfdae4662a
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-09-15 18:08:18 -0300

    feat: Inserção de class e constructor

---
+++ src/models/PrecoModel.js
@@ -0,0 +1,12 @@
+class PrecoModel{
+    // @param {string} = dia_semana
+    // @param {string} = valor
+    
+    constructor(dia_semana, valor){
+    this.dia_semana = dia_semana
+    this.valor = valor
+    }
+}     
+
+
+export default PrecoModel
