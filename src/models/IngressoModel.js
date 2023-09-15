ef2273ffa7b7ba76c9dea2f54f82796730e23afb
ericafernandaj <eriicafernandaj@gmail.com>
2023-09-15 18:36:22 -0300

    feat: Inserção da class e constructor do arquivo IngressoModel.js

---
+++ src/models/IngressoModel.js
@@ -0,0 +1,10 @@
+class IngressoModel {
+
+    // @param { string } = tipo_ingresso
+
+    construtor(tipo_ingresso) {
+        isso.tipo_ingresso = tipo_ingresso
+    }
+}
+
+export default IngressoModel
