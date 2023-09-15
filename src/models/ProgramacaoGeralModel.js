415f204cd1e42cbb6c3d874f44600b93f3f3de07
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-09-15 18:13:20 -0300

    feat: alteração de informação no constructor

--- src/models/ProgramacaoGeralModel.js
+++ src/models/ProgramacaoGeralModel.js
@@ -3,7 +3,7 @@ class ProgramacaoGeralModel{
 // @param {string} data_horario
 // @param {string} tipo_linguagem
 
-constructor(id_filme_fk, data_horario,tipo_linguagem){
+constructor(data_horario,tipo_linguagem){
 this.data_horaio = data_horario
 this.tipo_linguagem = tipo_linguagem
 }
