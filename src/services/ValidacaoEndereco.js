928f80b3b1fa27c2c16d0d2745fea7d526838f54
Lucas Rolando <lucasrls98@gmail.com>
2023-09-18 19:04:44 -0300

    feat: validação endereço.

---
+++ src/services/ValidacaoEndereco.js
@@ -0,0 +1,70 @@
+class ValidacaoEndereco {
+    /**
+     * Validação para o logradouro
+     * @param {string} logradouro - Logradouro do endereço.
+     * @returns {boolean} - true se o logradouro for válido, caso contrário, false.
+     */
+    static validaLogradouro(logradouro) {
+        return typeof logradouro === "string" && logradouro.length > 2;
+    }
+
+    /**
+     * Validação para o número do endereço
+     * @param {string} numero - Número do endereço.
+     * @returns {boolean} - true se o número for válido, caso contrário, false.
+     */
+    static validaNumero(numero) {
+        return typeof numero === "string" && numero.length > 0;
+    }
+
+    /**
+     * Validação para a cidade
+     * @param {string} cidade - Cidade do endereço.
+     * @returns {boolean} - true se a cidade for válida, caso contrário, false.
+     */
+    static validaCidade(cidade) {
+        return typeof cidade === "string" && cidade.length > 2;
+    }
+
+    /**
+     * Validação para a UF (Unidade Federativa)
+     * @param {string} uf - UF do endereço.
+     * @returns {boolean} - true se a UF for válida, caso contrário, false.
+     */
+    static validaUF(uf) {
+        return typeof uf === "string" && uf.length === 2;
+    }
+
+    /**
+     * Validação para o CEP (Código de Endereçamento Postal)
+     * @param {string} cep - CEP do endereço.
+     * @returns {boolean} - true se o CEP for válido, caso contrário, false.
+     */
+    static validaCEP(cep) {
+        // Implemente a validação específica para o formato de CEP desejado
+        // Por exemplo, você pode verificar o comprimento e o formato do CEP.
+        // Exemplo simplificado:
+        return typeof cep === "string" && cep.length === 8 && /^\d{8}$/.test(cep);
+    }
+
+    /**
+     * Validação para todos os campos do endereço
+     * @param {string} logradouro - Logradouro do endereço.
+     * @param {string} numero - Número do endereço.
+     * @param {string} cidade - Cidade do endereço.
+     * @param {string} uf - UF do endereço.
+     * @param {string} cep - CEP do endereço.
+     * @returns {boolean} - true se todos os campos do endereço forem válidos, caso contrário, false.
+     */
+    static validaCamposEndereco(logradouro, numero, cidade, uf, cep) {
+        const ehValido =
+            this.validaLogradouro(logradouro) &&
+            this.validaNumero(numero) &&
+            this.validaCidade(cidade) &&
+            this.validaUF(uf) &&
+            this.validaCEP(cep);
+        return ehValido;
+    }
+}
+
+export default ValidacaoEndereco;
