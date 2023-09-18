134aa7b8b286fa09f0fc98379a0e10c5c0e00462
Lucas Rolando <lucasrls98@gmail.com>
2023-09-18 19:03:49 -0300

    refactor: refazendo DAO.

--- src/DAO/DAO.js
+++ src/DAO/DAO.js
@@ -1,80 +1,110 @@
 import Database from "../database/Database.js";
 
-class DAO{
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3.Database('database.db');
+
+class DAO {
     /**
      * Método de inserção de dados
      * @param {string} query 
      * @param {Array<any>} data 
+     * @returns {Promise<{ error: boolean, lastInsertId?: number }>}
      */
-    static inserir(query, data){
-        return new Promise((resolve, reject)=>{
-            Database.run(query, data, (error)=>{
-                if(error){
-                    console.log(error)
-                    reject(error)
+    static inserir(query, data) {
+        return new Promise((resolve, reject) => {
+            db.run(query, data, function (error) {
+                if (error) {
+                    console.error(error);
+                    reject(error);
+                } else {
+                    resolve({ error: false, lastInsertId: this.lastID });
                 }
-                resolve({error:false})
-            })
-        })
+            });
+        });
     }
 
     /**
      * Método de busca de dados
-     * @param {string} entidade 
-     * @returns {any}
+     * @param {string} query 
+     * @returns {Promise<Array<any>>}
      */
-    static buscar(entidade){
-        const query = `
-        SELECT * FROM ${entidade};
-        `
-        return new Promise((resolve, reject)=>{
-            Database.all(query, (error, rows)=>{
-                if(error){
-                    console.log(error)
+    static buscar(query) {
+        return new Promise((resolve, reject) => {
+            db.all(query, (error, rows) => {
+                if (error) {
+                    console.error(error);
+                    reject(error);
                 } else {
-                    resolve(rows)
+                    resolve(rows);
                 }
-            })
-        })
+            });
+        });
     }
 
     /**
      * Método de busca de dados específicos através de um identificador
-     * @param {string} entidade 
+     * @param {string} query 
      * @param {string} id 
-     * @returns {any}
+     * @returns {Promise<any>}
      */
-    static buscarPorId(entidade, id){
-        return Database[entidade][id]
+    static buscarPorId(query, id) {
+        return new Promise((resolve, reject) => {
+            db.get(query, [id], (error, row) => {
+                if (error) {
+                    console.error(error);
+                    reject(error);
+                } else {
+                    resolve(row);
+                }
+            });
+        });
     }
 
     /**
      * Método de busca de dados específicos através do próprio dado
-     * @param {string} entidade 
+     * @param {string} query 
      * @param {string} dado 
-     * @returns 
+     * @returns {Promise<any>}
      */
-    static buscarDado(entidade, dado){
-        return Database[entidade][dado]
+    static buscarDado(query, dado) {
+        return new Promise((resolve, reject) => {
+            db.get(query, [dado], (error, row) => {
+                if (error) {
+                    console.error(error);
+                    reject(error);
+                } else {
+                    resolve(row);
+                }
+            });
+        });
     }
 
     /**
      * Método de deleção de dados específicos através de um identificador
-     * @param {string} entidade 
+     * @param {string} query 
      * @param {string} id 
      */
-    static deletarPorId(entidade, id){
-        delete Database[entidade][id]
+    static deletarPorId(query, id) {
+        db.run(query, [id], (error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
     }
 
     /**
      * Atualiza um registro específico na base de dados através de um identificador
-     * @param {string} entidade 
+     * @param {string} query 
      * @param {string} id 
      * @param {any} data 
      */
-    static atualizarPorId(entidade, id, data){
-        Database[entidade][id] = data
+    static atualizarPorId(query, id, data) {
+        db.run(query, [data.campo1, data.campo2, id], (error) => {
+            if (error) {
+                console.error(error);
+            }
+        });
     }
 }
 
