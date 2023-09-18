7049c52bbb9634def497b74af54ff9881deb5a2d
Lucas Rolando <lucasrls98@gmail.com>
2023-09-18 19:28:28 -0300

    feat: services - ValidacaoFilme

---
+++ src/services/ValidacaoFilme.js
@@ -0,0 +1,60 @@
+import FilmesDAO from "../DAO/FilmesDAO";
+
+class ValidacaoFilme {
+    /**
+     * Método para validar o nome de um filme
+     * @param {string} nome
+     * @returns {boolean}
+     */
+    static validaNome(nome) {
+        return typeof nome === "string" && nome.trim().length > 0;
+    }
+
+    /**
+     * Método para validar a linguagem original de um filme
+     * @param {string} linguagemOriginal
+     * @returns {boolean}
+     */
+    static validaLinguagemOriginal(linguagemOriginal) {
+        return typeof linguagemOriginal === "string" && linguagemOriginal.trim().length > 0;
+    }
+
+    /**
+     * Método para validar a classificação indicativa de um filme
+     * @param {string} classificacaoIndicativa
+     * @returns {boolean}
+     */
+    static validaClassificacaoIndicativa(classificacaoIndicativa) {
+        // Supondo que os valores válidos para classificacaoIndicativa sejam "Livre", "10", "12", "14", "16" ou "18"
+        return ["Livre", "10", "12", "14", "16", "18"].includes(classificacaoIndicativa);
+    }
+
+    /**
+     * Método para validar a duração de um filme no formato HH:MM
+     * @param {string} duracao
+     * @returns {boolean}
+     */
+    static validaDuracao(duracao) {
+        // Expressão regular para verificar o formato HH:MM (por exemplo, "02:30")
+        const regex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+        return regex.test(duracao);
+    }
+
+    /**
+     * Método para validar todos os campos de um filme
+     * @param {object} filmeData
+     * @returns {boolean}
+     */
+    static validaCamposFilme(filmeData) {
+        const { nome_filme, linguagem_original, classificacao_indicativa, duracao } = filmeData;
+
+        return (
+            this.validaNome(nome_filme) &&
+            this.validaLinguagemOriginal(linguagem_original) &&
+            this.validaClassificacaoIndicativa(classificacao_indicativa) &&
+            this.validaDuracao(duracao)
+        );
+    }
+}
+
+export default ValidacaoFilme;
